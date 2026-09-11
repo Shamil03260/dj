a4efc10e2df265717163541c7b5c87fb90b16fa4
Shamil03260 <ferideshamil1286@gmail.com>
2026-09-11 19:44:08 +0400

    7tfu

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, AbstractBaseUser
+from django.utils import timezone
 
 
 class Custom_User(AbstractUser):
