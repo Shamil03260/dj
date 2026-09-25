b666ca0f6d5c8a499c82783c9a850f1cb09ce540
Shamil03260 <ferideshamil1286@gmail.com>
2026-09-25 10:09:23 +0400

    3thd

--- admindashboard/models.py
+++ admindashboard/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Teacher(models.Model):
+    full_name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.full_name} {self.email}"
+    
+class courses(models.Model):
+    name = models.Model.CharField(max_length=100)
+    teacher = models.ForeignKey(
+        Teacher,
+        on_delete=models.CASCADE
+    )
+    
+    def __str__(self):
+        return {self.name}
+    
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    course = models.ForeignKey(
+        courses,
+        on_delete=models.CASCADE
+    )
+    
+    def __str__(self):
+        return {self.name}
